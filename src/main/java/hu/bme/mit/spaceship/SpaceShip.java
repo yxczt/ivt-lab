637af5cffe2478496d546315d989bd132f107700
unknown <elozsombor@gmail.com>
2021-04-16 23:35:16 +0200

    fireLaser comment change

--- src/main/java/hu/bme/mit/spaceship/SpaceShip.java
+++ src/main/java/hu/bme/mit/spaceship/SpaceShip.java
@@ -10,7 +10,7 @@ public interface SpaceShip {
   * Fires the laser or lasers of the ship
   *
   * @param firingMode how many lasers to fire
-  * @return was the firing successful
+  * @return was the firing successful or not
   */
   public boolean fireLaser(FiringMode firingMode);
 
